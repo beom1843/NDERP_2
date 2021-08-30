3e882606a25532d190649c22f9d1431dd52b1995
beom1843 <beom1843@gmail.com>
2021-08-30 21:01:03 +0900

    fix. java Configuration으로 수정함

---
+++ src/main/java/org/nderp2/config/RootConfig.java
@@ -0,0 +1,38 @@
+package org.nderp2.config;
+
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"org.nderp2.mapper"}) // mapper
+@ComponentScan(basePackages = {"org.nderp2.service"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource(){
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setUsername("nderp2");
+		hikariConfig.setPassword("1234");
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+}
